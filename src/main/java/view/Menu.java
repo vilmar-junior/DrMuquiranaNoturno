c0b1e9c80d22a07e5a022df68d645cc2fd3f5cf6
Aluno <Aluno@S202M01>
2019-07-26 21:52:37 -0300

    Novo commit

--- src/main/java/view/Menu.java
+++ src/main/java/view/Menu.java
@@ -5,33 +5,34 @@ import java.util.Scanner;
 public class Menu {
 
 	Scanner teclado = new Scanner(System.in);
-	
+
+	// teste
 	private static final int OPCAO_MENU_CADASTRO = 1;
 	private static final int OPCAO_MENU_RELATORIO = 2;
 	private static final int OPCAO_MENU_SAIR = 3;
-	
+
 	public void apresentarMenu() {
 		int opcao = this.apresentarOpcoesMenu();
-		while (opcao != OPCAO_MENU_SAIR){
-			switch(opcao){
-				case OPCAO_MENU_CADASTRO: {
-					MenuCadastro menuCadastro = new MenuCadastro();
-					menuCadastro.apresentarMenuCadastro();
-					break;
-				}
-				case OPCAO_MENU_RELATORIO: {
-					MenuRelatorio menuRelatorio = new MenuRelatorio();
-					menuRelatorio.apresentarMenuRelatorio();
-					break;
-				}
-				default: {
-					System.out.println("\nOpção Inválida");
-				}
+		while (opcao != OPCAO_MENU_SAIR) {
+			switch (opcao) {
+			case OPCAO_MENU_CADASTRO: {
+				MenuCadastro menuCadastro = new MenuCadastro();
+				menuCadastro.apresentarMenuCadastro();
+				break;
+			}
+			case OPCAO_MENU_RELATORIO: {
+				MenuRelatorio menuRelatorio = new MenuRelatorio();
+				menuRelatorio.apresentarMenuRelatorio();
+				break;
+			}
+			default: {
+				System.out.println("\nOpção Inválida");
+			}
 			}
 			opcao = this.apresentarOpcoesMenu();
 		}
 	}
-	
+
 	private int apresentarOpcoesMenu() {
 		System.out.println("Dr. Muquirana - Controle de Gastos");
 		System.out.println("\nOpções:");
