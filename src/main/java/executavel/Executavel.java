0577faa21713c954161c149100594f2a2f039107
Vilmar César Pereira Júnior <vilmar.junior@edu.sc.senac.br>
2019-09-14 22:31:27 -0300

    Ajustes

--- src/main/java/executavel/Executavel.java
+++ src/main/java/executavel/Executavel.java
@@ -1,6 +1,7 @@
 package executavel;
 
-import view.Menu;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Classe principal da aplicação Dr Muquirana.
@@ -12,24 +13,19 @@ public class Executavel {
 
 	public static void main(String[] args) {
 
-		Menu menu = new Menu();
-		menu.apresentarMenu();
+//		Menu menu = new Menu();
+//		menu.apresentarMenu();
 
-//		HashMap<String, UsuarioVO> mapUsuarios = new HashMap<String, UsuarioVO>();
-//		UsuarioVO joao = new UsuarioVO("João", "111");
-//		UsuarioVO jose = new UsuarioVO("José", "222");
-//		UsuarioVO maria = new UsuarioVO("Maria", "333");
-//
-//		mapUsuarios.put("555", jose);
-//		mapUsuarios.put(maria.getCpf(), maria);
-//		mapUsuarios.put(joao.getCpf(), joao);
-//
-//		Collection<UsuarioVO> valoresNoMap = mapUsuarios.values();
-//		Iterator<UsuarioVO> iterador = valoresNoMap.iterator();
-//
-//		while (iterador.hasNext()) {
-//			System.out.println(iterador.next());
-//		}
+		mostrarDataAtualFormatada();
 
+	}
+
+	public static void mostrarDataAtualFormatada() {
+		DateTimeFormatter formatadorDeData = DateTimeFormatter.ofPattern("dd-MM-yy");
+		LocalDate dataAtual = LocalDate.now();
+
+		String dataFormatada = dataAtual.format(formatadorDeData);
+
+		System.out.println(dataFormatada);
 	}
 }
